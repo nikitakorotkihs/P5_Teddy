98286656a271e43ded767c80b4669b50ca10c329
Nick K <nikitakorotkihs@gmail.com>
2021-10-21 16:51:12 +0200

    Product Page-added css box + JS

--- JavaScript/product-page.js
+++ JavaScript/product-page.js
@@ -1,7 +1,48 @@
-fetch('http://localhost:3000/api/teddies')
-    .then(res => res.blob())
-    .then(blob => {
-        let img = document.createElement('img');
-        img.src = URL.createObjectURL(blob);
-        document.querySelector('body').appendChild(img);
-    });
+let productId = "5be9c8541c9d440000665243";
+fetch(`http://localhost:3000/api/teddies/${productId}`)
+    .then(response => response.json())
+    .then(json => createProductCard(json));
+
+function createProductCard(product) {
+    let url = product.imageUrl;
+    // Define card element
+    let card = document.createElement("div");
+    card.classList.add("product-container");
+
+    let ProductImg = document.createElement("img");
+    ProductImg.classList.add("product-img");
+    ProductImg.src = url;
+
+    let product_info = document.createElement("div");
+    product_info.classList.add("product-info");
+
+    let product_name = document.createElement("div");
+    product_name.classList.add("product-name");
+    product_name.innerText = product.name;
+
+
+    let product_color = document.createElement("div");
+    product_color.classList.add("change-color");
+    product_color.innerText = product.color;
+
+    let product_price = document.createElement("div");
+    product_price.classList.add("product-price");
+    product_price.innerText = product.price;
+
+    let product_description = document.createElement("div");
+    product_description.classList.add("product-description");
+    product_description.innerText = product.description;
+
+
+    card.appendChild(ProductImg);
+    card.appendChild(product_info);
+    product_info.appendChild(product_name);
+    product_info.appendChild(product_price);
+    product_info.appendChild(product_description);
+
+
+    //Added 
+    // Identify the product section and insert card to that product section
+    let productSection = document.querySelector("#product-section");
+    productSection.appendChild(card);
+}
